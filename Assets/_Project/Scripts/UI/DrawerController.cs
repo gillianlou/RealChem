d4d2c2d5251b65dd28f268895e6663a99ed61c8a
Gillian Hughes <gillianhughes2017@gmail.com>
2020-09-19 13:26:31 -0400

    Add Basic UI interactions

---
+++ Assets/_Project/Scripts/UI/DrawerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RealChem.UI
+{
+    public class DrawerController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject closeDrawerButton;
+        private RectTransform RectTransform => transform as RectTransform;
+        public void Open() {
+            closeDrawerButton.SetActive(true);
+            RectTransform.anchoredPosition = new Vector2(RectTransform.anchoredPosition.x, 800);
+        }
+
+        public void Close(){
+            closeDrawerButton.SetActive(false);
+            RectTransform.anchoredPosition = new Vector2(RectTransform.anchoredPosition.x, 0);
+        }
+        
+    }
+}
---
+++ Assets/_Project/Scripts/UI/ElementIcon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RealChem.UI{
+    public class ElementIcon : MonoBehaviour
+    {
+        [SerializeField]
+        private string elementName;
+        public void AddElement(){
+              Debug.Log($"Add element {elementName} to the world");  
+        }
+
+    }
+}
+
