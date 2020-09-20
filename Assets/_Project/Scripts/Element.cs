f9b84ff47faa0b8365a61354d27b4e1dbc9e4ab5
Gillian Hughes <gillianhughes2017@gmail.com>
2020-09-19 20:17:58 -0400

    Element prefabs

---
+++ Assets/_Project/Scripts/Element.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RealChem{
+    public class Element : MonoBehaviour
+    {
+        [SerializeField]
+        private string symbol;
+
+    }
+}
--- Assets/_Project/Scripts/UI/ElementIcon.cs
+++ Assets/_Project/Scripts/UI/ElementIcon.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 namespace RealChem.UI{
     public class ElementIcon : MonoBehaviour
     {
+        private static readonly Vector2 Min = new Vector2(-2.5f, -1.5f);
+        private static readonly Vector2 Max = new Vector2(2.5f, 8);
         [SerializeField]
-        private string elementName;
+        private Element element;
         public void AddElement(){
-              Debug.Log($"Add element {elementName} to the world");  
+            var x = Random.Range(Min.x, Max.x);
+            var z = Random.Range(Min.y, Max.y);
+            var position = new Vector3(x, 0.5f, z);
+
+            Instantiate(element, position, Quaternion.identity);
         }
 
     }
